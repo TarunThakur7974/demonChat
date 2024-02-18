54b286e8dcea95969747946070cc2122d29491e4
TarunThakur7974 <tarunsinghthakur6261@gmail.com>
2024-02-18 14:44:09 +0530

    third test

--- script.js
+++ script.js
@@ -3,28 +3,27 @@ const container = document.getElementById('container')
 const input = document.getElementById('input')
 
 const socket = new WebSocket('wss://dummychat-lwq3.onrender.com');
+// const socket = new WebSocket('ws://localhost:5000');
+
+let a = prompt('Enter your name');
 
 socket.addEventListener('open', (event) => {
-    console.log(event, "open");
-});
-socket.addEventListener('connection', (event) => {
-    console.log(event, "connect");
+    socket.send(a + " Joined Chat")
+
 });
 
 socket.addEventListener('message', (event) => {
     container.innerHTML += `<p>${event.data}</p>`
-
 });
 
 socket.addEventListener('close', (event) => {
     console.log('Connection closed');
 });
 
-// Sending a message to the server
 function sendMessage() {
     const message = input.value
-    socket.send(message);
-    form.reset()
+    socket.send(a +" : " + message);
+    form.reset();
 
 }
 
